7f03ab873778bea27271ad5f4b5d215b06085181
michikusa <michikusa.okuribito@gmail.com>
2018-08-16 19:02:13 +0900

    Dog yearをおわらせました

---
+++ Dog_year.js
@@ -0,0 +1,8 @@
+const myAge =29;
+let earlyYears = 2;
+earlyYears = 10.5*earlyYears;
+let laterYears = myAge-2;
+laterYears *=4;
+myAgeInDogYears = earlyYears + laterYears;
+myName = "Hiro".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
